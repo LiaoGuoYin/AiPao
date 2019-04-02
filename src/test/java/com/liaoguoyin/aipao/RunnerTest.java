080d8052077e49b4d4f08ee9b0502d91c146afe6
LiaoGuoYin <liaoguoyin@live.com>
2019-04-02 23:55:29 +0800

    更新简单的异常提示

--- src/test/java/com/liaoguoyin/aipao/RunnerTest.java
+++ src/test/java/com/liaoguoyin/aipao/RunnerTest.java
@@ -8,22 +8,30 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class RunnerTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AipaoClinet clinet = new AipaoClinet();
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("请输入IMEICode: ");
-        clinet.imeiLogin(scanner.nextLine());//4cda88bcc04a4b8ab5fe9cfe39bec6ef
-        clinet.getBasicInfo();
-        clinet.running();
-        clinet.uploadRecord();
 
-        System.out.println("Success, 点击查看所有记录: http://sportsapp.aipao.me/Manage/UserDomain_SNSP_Records.aspx/MyResutls?userId=" + clinet.UserId);
-        System.out.println("正在输出本次跑步记录，请按任意键退出...");
-        File file = new File("output.txt");
-        FileWriter fileWriter = new FileWriter(file, true);
-        fileWriter.write(clinet.output.toString());
-        fileWriter.close();
+        try {
+            clinet.imeiLogin(scanner.nextLine());
+            clinet.getBasicInfo();
+            clinet.running();
+            clinet.uploadRecord();
+
+            System.out.println("Success, 点击查看所有记录: http://sportsapp.aipao.me/Manage/UserDomain_SNSP_Records.aspx/MyResutls?userId=" + clinet.UserId);
+            System.out.println("正在输出本次跑步记录，请按任意键退出...");
+            File file = new File("output.txt");
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(clinet.output.toString());
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("输入输出异常，请检查脚本是否有读写权限");
+        } catch (NullPointerException e) {
+            System.out.println("空指针异常，可能是IMEICode失效");
+        }
+
         scanner.nextLine();
+
     }
 }
