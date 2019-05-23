2a2af37469582a1da4efd0e3efa5ecab9d27eac8
LiaoGuoYin <liaoguoyin@live.com>
2019-05-23 23:51:55 +0800

    实现直接传参，方便批量化（毫无技术含量）

--- src/test/java/com/liaoguoyin/aipao/RunnerTest.java
+++ src/test/java/com/liaoguoyin/aipao/RunnerTest.java
@@ -9,29 +9,39 @@ import java.util.Scanner;
 
 public class RunnerTest {
     public static void main(String[] args) {
+        int pauseTime = 5000;
+        String imeiCode;
         AipaoClinet clinet = new AipaoClinet();
         Scanner scanner = new Scanner(System.in);
         System.out.println("LGY 太强了，请输入IMEICode: ");
 
+        if (args.length > 0){
+            imeiCode = args[0];
+            pauseTime = 0;
+        } else {
+            imeiCode = scanner.nextLine();
+        }
+
         try {
-            clinet.imeiLogin(scanner.nextLine());
+            clinet.imeiLogin(imeiCode);
             clinet.getBasicInfo();
             clinet.running();
             clinet.uploadRecord();
 
             System.out.println("Success, 点击查看所有记录: http://sportsapp.aipao.me/Manage/UserDomain_SNSP_Records.aspx/MyResutls?userId=" + clinet.UserId);
-            System.out.println("正在输出本次跑步记录，请按任意键退出...");
+            System.out.println("正在输出记录，5s 后退出..");
             File file = new File("output.txt");
             FileWriter fileWriter = new FileWriter(file, true);
             fileWriter.write(clinet.output.toString());
             fileWriter.close();
+            Thread.sleep(pauseTime);
         } catch (IOException e) {
             System.out.println("输入输出异常，请检查脚本是否有读写权限");
         } catch (NullPointerException e) {
-            System.out.println("空指针异常，可能是IMEICode失效");
+            System.out.println(imeiCode + " 空指针异常，可能是IMEICode失效");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        scanner.nextLine();
 
     }
 }
